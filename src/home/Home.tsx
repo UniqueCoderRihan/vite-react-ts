ff1fdd3b168470b35d9e2db78ad88dba44b75251
uniquecoderrihan <rihanthedev@gmail.com>
2023-12-19 00:32:26 +0600

    navbar added

---
+++ src/home/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './home.css'
+
+const Home = () => {
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <ul>
+                    <li>Home</li>
+                    <li>Blog</li>
+                    <li>News</li>
+                    <li>Javascript</li>
+                    <li>Next-Js</li>
+                    <li>Redux</li>
+                    <li>TypeScript</li>
+                    <li>Nodejs</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
