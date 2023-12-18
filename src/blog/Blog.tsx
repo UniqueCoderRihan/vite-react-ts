b03276327c71dab81938cd585a01dca79ea588bc
uniquecoderrihan <rihanthedev@gmail.com>
2023-12-19 01:12:58 +0600

    data fetched

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './../App.css'
 import blogdata from '../../public/blog.json';
 
-type Blog ={
-    id: number,
-    title: string,
-    cover: string,
-    author: string
-}
+
 
 const Blog = () => {
+    type Blog ={
+        id: number,
+        title: string,
+        cover: string,
+        author: string
+    }
+    const [fakeDb,setFakeDb] = useState([]);
     
+    useEffect(()=>{
+
+        fetch('/public/blog.json').then(res=>res.json()).then(data=>setFakeDb(data))
+    },[])
     return (
         <div className='container'>
             <div className='blog'>
-            {blogdata.map((blog: Blog)=>
+            {fakeDb.map((blog: Blog)=>
             <div className="card" key={blog.id}>
             <img src={blog.cover} alt="" />
             <div className="details">
