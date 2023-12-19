7598a4e731b75b38f369771f5ad87802f4ca9ab5
uniquecoderrihan <rihanthedev@gmail.com>
2023-12-19 14:48:45 +0600

    react-router added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import Home from './home/Home'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 // Todo: Next time you need to install react-router,
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home/>
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
